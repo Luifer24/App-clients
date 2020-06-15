ba9d25356b4a3086a56ff6208df92118a5318768
Luifer <luifer.ubeda@gmail.com>
2020-06-15 16:49:30 +0200

    Añadimos la funcion que añade clientes y la funcion que añade una coma

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
 clients = 'pablo, ricardo, '
 
+def create_client(client_name):
+    global clients
 
+    clients += client_name
+    _add_comma()
+
+
+def _add_comma():
+    global clients
+
+    clients += ','
 
 if __name__ == "__main__":
-    clients += 'david'
+    create_client('David')
     print(clients)
