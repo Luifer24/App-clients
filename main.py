af5f671325f5654c22f61392f18115f387256c7d
Luifer <luifer.ubeda@gmail.com>
2020-06-15 16:53:09 +0200

    Creamos la funcion list_clients que nos listara en consolo los clientes que tenemos

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ def create_client(client_name):
     _add_comma()
 
 
+def list_clients():
+    global clients
+
+    print(clients)
+
 def _add_comma():
     global clients
 
     clients += ','
 
 if __name__ == "__main__":
+    list_clients()
+
     create_client('David')
-    print(clients)
+    
+    list_clients
