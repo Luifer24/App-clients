001a0516742cf9387feef8bd58654f86cb20ec02
Luifer <luifer.ubeda@gmail.com>
2020-06-15 19:40:30 +0200

    Añadimos listas al progama para mejorarlo

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
+import sys
 
-clients = 'pablo, ricardo, '
+
+clients = ['pablo', 'ricardo']
 
 def create_client(client_name):
     global clients
 
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+        clients.append(client_name)
     else:
         print('Cleint already is in the client\'s list')
 
 
 def list_clients():
-    global clients
-
-    print(clients)
+    for idx, client in enumerate(clients):
+        print('{}: {}'.format(idx, client))
+    
 
 
 def update_client(client_name, updated_client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', updated_client_name + ',')
+        index = clients.index(client_name)
+        clients[index] = update_name
     else:
         print('Client is not in client\'s list')
 
@@ -30,25 +32,19 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', '')
+        clients.remove(client_name)
     else:
         print('Client is not in client\'s list')
 
 
 def search_client(client_name):
-    clients_list = clients.split(',')
-
-    for client in clients_list:
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
 
 
-def _add_comma():
-    global clients
-
-    clients += ','
 
 
 def _print_welcome():
@@ -63,7 +59,19 @@ def _print_welcome():
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+            sys.exit()
+    
+    return client_name
 
 if __name__ == "__main__":
     _print_welcome()
