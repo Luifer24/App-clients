d0ac8de7cdbdf269dd9b719e4b180b9785b8a2e0
Luifer <luifer.ubeda@gmail.com>
2020-06-15 17:04:54 +0200

    Creamos el welcome con la eleccion a elegir, tambien si un cliente está ya o no en la lista con condicionales

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ clients = 'pablo, ricardo, '
 def create_client(client_name):
     global clients
 
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Cleint already is in the client\'s list')
 
 
 def list_clients():
@@ -18,9 +21,26 @@ def _add_comma():
 
     clients += ','
 
-if __name__ == "__main__":
-    list_clients()
 
-    create_client('David')
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
+
+if __name__ == "__main__":
+    _print_welcome()
     
-    list_clients
+    command= input()
+
+    if command == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
+
